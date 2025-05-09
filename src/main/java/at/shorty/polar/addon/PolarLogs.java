7b87ffec55b97b25f97b33e2b8946f901bc3cd8c
ShortyDev <shortydev8274@gmail.com>
2025-05-09 23:13:27 +0200

    bump version to 2.5.1

--- src/main/java/at/shorty/polar/addon/PolarLogs.java
+++ src/main/java/at/shorty/polar/addon/PolarLogs.java
@@ -14,7 +14,7 @@ import top.polar.api.loader.LoaderApi;
 import java.lang.reflect.Field;
 import java.util.Set;
 
-@Plugin(name = "PolarLogs", version = "2.5.0")
+@Plugin(name = "PolarLogs", version = "2.5.1")
 @Author("ShortyDev")
 @Dependency("PolarLoader")
 public class PolarLogs extends JavaPlugin {
